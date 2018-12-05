192bd52e6f662f79c507126a55e8f4c00cf0fd87
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-12-05 15:57:22 -0800

    Move to latest version
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- dckr.rb
+++ dckr.rb
@@ -2,13 +2,13 @@ class Dckr < Formula
   desc "ShArt is a load of shcript"
   homepage "https://github.com/dubo-dubon-duponey/dckr"
   url "https://github.com/dubo-dubon-duponey/dckr.git",
-      :tag => "v0.0.1",
-      :revision => "2321cb2b2a1db6f56edbff654954de71f45e8781"
+      :tag => "v0.0.2",
+      :revision => "af40034a6cd0a9d5414bb51c62d0fdd978205674"
   head "https://github.com/dubo-dubon-duponey/dckr.git"
 
   def install
     bin.install "dckr"
-    bin.install Dir["Dockerfile.*"].select { |f| f }
+    bin.install Dir["*Dockerfile*"].select { |f| f }
   end
 
 end
