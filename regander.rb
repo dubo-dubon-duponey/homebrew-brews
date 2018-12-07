709df53a610364caa5de3ee589a6b93036fcbe02
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-12-06 20:37:59 -0800

    Adding regander
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

---
+++ regander.rb
@@ -0,0 +1,21 @@
+class Regander < Formula
+  desc "Regander! Make them registries behave!"
+  homepage "https://github.com/dubo-dubon-duponey/regander"
+  url "https://github.com/dubo-dubon-duponey/regander.git",
+      :tag => "v0.0.1",
+      :revision => "53195392d89ddbc1b407bb770905153a5f2f48e4"
+  head "https://github.com/dubo-dubon-duponey/regander.git"
+
+  depends_on "jq"
+  # depends_on "ffmpeg"
+
+  def install
+    system "make" "build", *args
+    bin.install Dir["bin/*"].select { |f| File.executable? f }
+    # bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "make", "test"
+  end
+end
