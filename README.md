b13e3bba4e5f18282d49dd83403aedec8da02a11
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2024-02-12 13:20:31 -0800

    CLeanup

--- README.md
+++ README.md
@@ -9,15 +9,6 @@ brew install dubo-dubon-duponey/brews/terraform-provider-gandi
 ```
 
 
-Mumbrew:
-
-> Daily auto-updater for brew formulas with notifications.
-
-```
-brew install dubo-dubon-duponey/brews/mumbrew
-brew services start mumbrew
-```
-
 SH-art:
 
 > Shell-script framework to ease developing fancy pieces of shcripts
@@ -28,7 +19,7 @@ brew install dubo-dubon-duponey/brews/sh_art
 
 Dckr:
 
-> helper to transparently test and run your local project inside docker containers
+> develop on any linux, wherever you are
 
 ```
 brew install dubo-dubon-duponey/brews/dckr
--- dckr.rb
+++ dckr.rb
@@ -1,9 +1,11 @@
 class Dckr < Formula
   desc "dckr, docker dev helper"
   homepage "https://github.com/dubo-dubon-duponey/dckr"
+  version "dev"
   url "https://github.com/dubo-dubon-duponey/dckr.git",
-      :tag => "v1.2.0",
-      :revision => "bf794913110c3fb8bb91403319da2abf88ab9b2b"
+      :revision => "5a8998636acb4a8aec289b090f4583e430965e12"
+      #      :tag => "v1.2.0",
+      #:revision => "bf794913110c3fb8bb91403319da2abf88ab9b2b"
   head "https://github.com/dubo-dubon-duponey/dckr.git", :branch => "master"
 
   def install
@@ -14,5 +16,4 @@ class Dckr < Formula
   test do
     system "./test.sh"
   end
-
 end
